7f927cdc12079633197fc9825058155e7941cd8d
3490418751@qq.com <bsn43025@163.com>
2026-03-09 20:19:05 +0800

    Little change

--- src/ERyder.java
+++ src/ERyder.java
@@ -11,38 +11,44 @@ public class ERyder {
         this.isAvailable = isAvailable;
         this.kmDriven = kmDriven;
     }
-    public ERyder()
-    {
+    public ERyder(){
     }
-    public void ride()
-    {
-        if(isAvailable == true && batteryLevel >=20)
-        {
-           System.out.println("The bike is available.");
+    public void ride(){
+        if(isAvailable == true && batteryLevel >= 10){
+           System.out.println("You can ride this bike.");
         }
-        else
-        {
-            System.out.println("The bike is not available.");
+        else{
+            System.out.println("You can't ride this bike. Please change one.");
         }
     }
-    public void printBikeDetails()
-    {
-        System.out.println("The bike ID is " + bikeID);
-        System.out.println("The battery level is " + batteryLevel);
+    public void printBikeDetails(){
+        System.out.println("The bike's ID is " + bikeID+".");
+        System.out.println("The battery level is " + batteryLevel+"%.");
         if(isAvailable){System.out.println("The bike is available.");}
         else {System.out.println("The bike is not available.");}
-        System.out.println("Its distance travelled in " + kmDriven + " km.");
+        System.out.println("The bike's distance travelled in " + kmDriven + " km.");
     }
+
+    //getters
     public String getBikeID(){
         return bikeID;
     }
-    public int geBatteryLevel(){
+    public int getBatteryLevel(){
         return batteryLevel;
     }
+    //not necessary
+    public boolean getIsAvailable(){
+        return isAvailable;
+    }
+    public float getKmDriven(){
+        return kmDriven;
+    }
+    //
+    //setters
     public void setBikeID(String bikeID){
         this.bikeID = bikeID;
     }
-    public void geBatteryLevel(int batteryLevel){
+    public void setBatteryLevel(int batteryLevel){
         if(batteryLevel>=0 && batteryLevel<=100){this.batteryLevel = batteryLevel;}
         else {System.out.println("The batteryLevel must be from 0 to 100");}
     }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         ERyder bike1 = new ERyder();
+        bike1.kmDriven = 1;
+        bike1.setBatteryLevel(20);
         bike1.printBikeDetails();
         ERyder bike2 = new ERyder("1234xx",98,true,23.5f);
         bike2.printBikeDetails();
+        bike2.ride();
+        bike1.kmDriven = 1;
     }
 }
