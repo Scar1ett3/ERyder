df9676255adc223f8ae65fc32d9a50fff87a3c85
3490418751@qq.com <bsn43025@163.com>
2026-03-10 18:22:45 +0800

    New version

--- src/ERyder.java
+++ src/ERyder.java
@@ -4,14 +4,53 @@ public class ERyder {
     public boolean isAvailable;
     public float kmDriven;
 
+    private static final String COMPANY_NAME = "ERyder";
+    private static final double BASE_FARE = 1.0;
+    private static final double PER_MINUTE_FARE = 0.5;
+
+    private final String LINKED_ACCOUNT;
+    private final long LINKED_PHONE_NUMBER;
+
+    private int totalUsageInMinutes;
+    private double totalFare;
+
+
     public ERyder(String bikeID, int batteryLevel, boolean isAvailable, float kmDriven)
     {
         this.bikeID = bikeID;
         this.batteryLevel = batteryLevel;
         this.isAvailable = isAvailable;
         this.kmDriven = kmDriven;
+        LINKED_ACCOUNT = "greetg";
+        LINKED_PHONE_NUMBER = 1234567;
+
     }
-    public ERyder(){}
+
+    public ERyder(String bikeID, int batteryLevel, boolean isAvailable, float kmDriven, String linkedAccount, long linkedPhoneNumber){
+        this.bikeID = bikeID;
+        this.batteryLevel = batteryLevel;
+        this.isAvailable = isAvailable;
+        this.kmDriven = kmDriven;
+        LINKED_ACCOUNT = linkedAccount;
+        LINKED_PHONE_NUMBER = linkedPhoneNumber;
+    }
+
+    public void printRideDetails(int usageInMinutes){
+        totalUsageInMinutes = usageInMinutes;
+        System.out.println("======== Ride Details ========");
+        System.out.println("The linked account is "+LINKED_ACCOUNT+".");
+        System.out.println("The linked phone number is "+LINKED_PHONE_NUMBER+".");
+        System.out.println("The bike ID is "+bikeID+".");
+        System.out.println("The usage in minutes is "+totalUsageInMinutes+".");
+        System.out.println("The total fare is "+calculateFare(usageInMinutes)+".");
+        System.out.println("==============================\n");
+    }
+
+    private double calculateFare(int usageInMinutes){
+        totalFare = BASE_FARE + (PER_MINUTE_FARE*usageInMinutes);
+        return usageInMinutes*PER_MINUTE_FARE+BASE_FARE;
+    }
+
     public void ride(){
         if(isAvailable == true && batteryLevel >= 10){
            System.out.println("You can ride this bike.");
@@ -20,12 +59,15 @@ public class ERyder {
             System.out.println("You can't ride this bike. Please change one.");
         }
     }
+
     public void printBikeDetails(){
+        System.out.println("======== Bike Details ========");
         System.out.println("The bike's ID is " + bikeID+".");
         System.out.println("The battery level is " + batteryLevel+"%.");
         if(isAvailable){System.out.println("The bike is available.");}
         else {System.out.println("The bike is not available.");}
         System.out.println("The bike's distance travelled in " + kmDriven + " km.");
+        System.out.println("==============================\n");
     }
 
     //getters
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        ERyder bike1 = new ERyder();
-        bike1.kmDriven = 1;
-        bike1.setBatteryLevel(20);
+        ERyder bike1 = new ERyder("1744ef",70,true,765.2f,"ngc232123",193848323);
         bike1.printBikeDetails();
+        bike1.printRideDetails(52);
         ERyder bike2 = new ERyder("1234xx",98,true,23.5f);
         bike2.printBikeDetails();
+        bike2.printRideDetails(30);
         bike2.ride();
         bike1.kmDriven = 1;
     }
